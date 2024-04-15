d86d0e36989458445656476ce51988886aee79c0
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2024-04-15 13:18:04 +0530

    ejs template code added

---
+++ EJSDIR/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
+
+app.set("view engine", "ejs"); //template engine enable you to use static template files in applications
+app.set("views", path.join(__dirname, "/views")); //parent theke access korte parbo
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+app.get("/roll_dice", (req, res) => {
+  let num = Math.floor(Math.random() * 6) + 1;
+  res.render("roll_dice.ejs", { diceVal: num });
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
