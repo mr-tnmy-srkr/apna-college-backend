c0dd3ea31a5b4e8f4c9988986ccf1ce9b5c93493
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2024-04-19 19:41:57 +0530

    quora post app complete (CRUD)

--- REST/data.js
+++ REST/data.js
@@ -1,13 +1,21 @@
+const { v4: uuidv4 } = require('uuid');
+
 const post = [
   {
+    id:uuidv4(),
+    // id:uuid,
     username: "apnacollege",
     content: "I love coding",
   },
   {
+    id:uuidv4(),
+    // id:uuid,
     username: "shradha",
     content: "Hard work is important",
   },
   {
+    id:uuidv4(),
+    // id:uuid,
     username: "rahul",
     content: "Got selected in intern",
   },
--- REST/index.js
+++ REST/index.js
@@ -2,19 +2,57 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
-const posts = require("./data");
+let allPosts = require("./data");
+var methodOverride = require("method-override");
+
 // console.log(data);
 app.use(express.urlencoded({ extended: true }));
 
-app.set("view engine", "ejs")
-app.set("views",path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/posts", (req, res) => {
-    res.render("index.ejs",{posts})
-})
+// override with POST having ?_method=DELETE ot PATCH
+app.use(methodOverride("_method"));
 
-app.listen(port, ()=>{
-    console.log(`listening on port : ${port}`);
-})
+app.get("/posts", (req, res) => {
+  res.render("index.ejs", { posts: allPosts });
+});
+app.get("/posts/new", (req, res) => {
+  res.render("new.ejs");
+});
+app.post("/posts", (req, res) => {
+  let { username, content } = req.body;
+  let id = crypto.randomUUID();
+  allPosts.push({ id, username, content });
+  res.redirect("/posts");
+});
+app.get("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let post = allPosts.find((p) => id == p.id);
+  res.render("show.ejs", { post });
+});
+app.patch("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let newContent = req.body.content;
+  //   console.log(newContent);
+  let post = allPosts.find((p) => id == p.id);
+  post.content = newContent;
+  //   console.log(post);
+  res.redirect("/posts");
+});
+app.get("/posts/:id/edit", (req, res) => {
+  let { id } = req.params;
+  let post = allPosts.find((p) => id == p.id);
+  res.render("edit.ejs", { post });
+});
+app.delete("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  allPosts = allPosts.filter((p) => id !== p.id);
+  console.log(post);
+  res.redirect("/posts");
+});
+app.listen(port, () => {
+  console.log(`listening on port : ${port}`);
+});
