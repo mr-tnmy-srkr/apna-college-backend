03728d9371a970a893c5dc2e33a55fb9700105d5
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2024-04-19 11:11:37 +0530

    quora post template added

---
+++ REST/data.js
@@ -0,0 +1,15 @@
+const post = [
+  {
+    username: "apnacollege",
+    content: "I love coding",
+  },
+  {
+    username: "shradha",
+    content: "Hard work is important",
+  },
+  {
+    username: "rahul",
+    content: "Got selected in intern",
+  },
+];
+module.exports = post;
--- REST/index.js
+++ REST/index.js
@@ -2,16 +2,17 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
-
+const posts = require("./data");
+// console.log(data);
 app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs")
-app.set("view",path.join(__dirname, "views"));
+app.set("views",path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-    res.send("serving working well")
+app.get("/posts", (req, res) => {
+    res.render("index.ejs",{posts})
 })
 
 app.listen(port, ()=>{
